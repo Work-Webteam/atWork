1a183d1470a5eececcfb95841126fb9f84ed9478
twerdal <thayne.werdal@gov.bc.ca>
2019-03-12 23:00:15 +0000

    Setting up admin field - we now have an ajax fieldset that returns optional fields.

--- web/modules/custom/atwork_idir_update/src/Form/AtworkIdirUpdateAdminSettingsForm.php
+++ web/modules/custom/atwork_idir_update/src/Form/AtworkIdirUpdateAdminSettingsForm.php
@@ -55,20 +55,24 @@ class AtworkIdirUpdateAdminSettingsForm extends ConfigFormBase {
       '#size' => 64,
       '#default_value' => $config->get('idir_login_password'),
     ];
+    // We need a way to count additional fields if any were saved, then add them to the form via Ajax.
     $form['idir_generate_fields'] = [
-      '#type' => 'button',
+      '#type' => 'submit',
       '#title' => $this->t('Generate Fields'),
+      '#value' => $this->t('Generate Fields'),
       '#description' => $this->t('Generate the fields available in the data file'),
       '#default_value' => $config->get('idir_generate_fields'),
+      '#ajax' => [
+        'callback' => [$this, 'idirGenerateFields'],
+        'wrapper' => 'names-fieldset-wrapper',
+      ],
     ];
-    $form['idir_save'] = [
-      '#type' => 'button',
-      '#title' => $this->t('Save'),
-      '#description' => $this->t('Save any administration changes you have made'),
-      '#default_value' => $config->get('idir_save'),
+    $form['names_fieldset'] = [
+      '#type' => 'fieldset',
+      '#title' => $this->t('People coming to picnic'),
+      '#prefix' => '<div id="names-fieldset-wrapper">',
+      '#suffix' => '</div>',
     ];
-    // TODO: We need a way to count additional fields if any were saved, then we need to print them out here.
-    // LABEL == column name, dropdown = available user fields.
     return parent::buildForm($form, $form_state);
   }
 
@@ -84,14 +88,32 @@ class AtworkIdirUpdateAdminSettingsForm extends ConfigFormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     parent::submitForm($form, $form_state);
-    // TODO: Gather all additional fields (if any) and save from form-state dropdown.
+    // TODO: Gather all additional fields (if any) and save from form-state dropdown (from fieldset).
     $this->config('atwork_idir_update.atworkidirupdateadminsettings')
       ->set('idir_ftp_location', $form_state->getValue('idir_ftp_location'))
       ->set('idir_login_name', $form_state->getValue('idir_login_name'))
       ->set('idir_login_password', $form_state->getValue('idir_login_password'))
       ->set('idir_generate_fields', $form_state->getValue('idir_generate_fields'))
-      ->set('idir_save', $form_state->getValue('idir_save'))
       ->save();
   }
 
+
+  // LABEL == column name, dropdown = available user fields.
+  public function idirGenerateFields(array &$form, FormStateInterface $form_state){
+    // Add a fields to our config if required
+    // TODO: Function to grab just the .csv labels and return them
+    // TODO: Foreach the labels, then add them to the fieldset
+    $config = $this->config('atwork_idir_update.atworkidirupdateadminsettings');
+    ksm('We did it!');
+    $form['names_fieldset']['idir_test_fields'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Test field'),
+      '#description' => $this->t('Test field'),
+      '#maxlength' => 128,
+      '#size' => 64,
+      '#default_value' => $config->get('idir_test_fields'),
+    ];
+
+    return $form['names_fieldset'];
+  }
 }
