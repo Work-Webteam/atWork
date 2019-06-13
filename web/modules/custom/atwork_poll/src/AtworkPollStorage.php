c8f9d3ba01ed77e3474a2a09f45e7ab1a5405c66
twerdal <thayne.werdal@gov.bc.ca>
2019-06-13 15:41:30 +0000

    Update to getExpiredPolls function so that it uses start date rather than creation date

--- web/modules/custom/atwork_poll/src/AtworkPollStorage.php
+++ web/modules/custom/atwork_poll/src/AtworkPollStorage.php
@@ -3,6 +3,7 @@
 namespace Drupal\atwork_poll;
 
 use Exception;
+use Drupal\poll\PollStorage;
 
 /**
  * Controller class for polls.
@@ -10,22 +11,30 @@ use Exception;
  * This extends the default content entity storage class,
  * adding required special handling for poll entities.
  */
-class AtworkPollStorage {
+class AtworkPollStorage extends PollStorage {
 
   /**
    * {@inheritdoc}
    */
-  public function getPollsToPublish() {
+  public static function getPollsToPublish() {
     $connection = \Drupal::database();
     $result = NULL;
     try {
-      $query = $connection->query("SELECT poll_field_data.id FROM {poll_field_data} JOIN {poll__field_poll_publishing_date} ON poll_field_data.id = poll__field_poll_publishing_date.entity_id WHERE UNIX_TIMESTAMP(poll__field_poll_publishing_date.field_poll_publishing_date_value) <  UNIX_TIMESTAMP() AND (((poll_field_data.created + poll_field_data.runtime) < UNIX_TIMESTAMP() AND runtime <> 0 ) OR runtime = 0) AND status = 0");
+      $query = $connection->query("SELECT poll_field_data.id FROM {poll_field_data} JOIN {poll__field_poll_publishing_date} ON poll_field_data.id = poll__field_poll_publishing_date.entity_id WHERE UNIX_TIMESTAMP(poll__field_poll_publishing_date.field_poll_publishing_date_value) <  UNIX_TIMESTAMP() AND (((UNIX_TIMESTAMP(poll__field_poll_publishing_date.field_poll_publishing_date_value) + poll_field_data.runtime) < UNIX_TIMESTAMP() AND runtime <> 0 ) OR runtime = 0) AND status = 0");
       $result = \Drupal::entityTypeManager()->getStorage('poll')->loadMultiple($query->fetchCol());
     }
     catch (Exception $e) {
       \Drupal::logger('type')->error($e->getMessage());
     }
     return $result;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getExpiredPolls() {
+    $query = $this->database->query("SELECT poll_field_data.id FROM {poll_field_data} JOIN {poll__field_poll_publishing_date} ON poll_field_data.id = poll__field_poll_publishing_date.entity_id WHERE (UNIX_TIMESTAMP() > (UNIX_TIMESTAMP(poll__field_poll_publishing_date.field_poll_publishing_date_value) + poll_field_data.runtime)) AND status = 1 AND runtime <> 0");
+    return $this->loadMultiple($query->fetchCol());
   }
 
 }
