3c3e6184e94287c5aaeca8a6cc55222ab53ca117
twerdal <thayne.werdal@gov.bc.ca>
2019-07-23 22:37:43 +0000

    Refactoring code to better meet Drupal coding guidlines

--- web/modules/custom/atwork_group/src/Breadcrumb/atworkGroupBreadcrumbBuilder.php
+++ web/modules/custom/atwork_group/src/Breadcrumb/atworkGroupBreadcrumbBuilder.php
@@ -12,7 +12,7 @@ use Drupal\Core\Link;
 /**
  * Define class and implement BreadcrumbBuilderInterface.
  */
-class atworkGroupBreadcrumbBuilder implements BreadcrumbBuilderInterface {
+class AtworkGroupBreadcrumbBuilder implements BreadcrumbBuilderInterface {
 
   /**
    * {@inheritdoc}
@@ -49,102 +49,113 @@ class atworkGroupBreadcrumbBuilder implements BreadcrumbBuilderInterface {
     // If this doesn't apply to the route, return false.
     return FALSE;
   }
-	
-	/**
-	 * {@inheritdoc}
-	 */
-	public function build(RouteMatchInterface $route_match) {
-		
-		// Define a new object of type Breadcrumb
-		$breadcrumb = new Breadcrumb();
-		
-		// Build out the breadcrumb
-		// Add a link to the homepage as our first crumb.
-		$breadcrumb->addLink(Link ::createFromRoute('Home', '<front>'));
 
-		// If this is a view page (ie related content view) handle it differently than a node type.
-		if((\Drupal::routeMatch()->getParameter('view_id')) && (\Drupal::routeMatch()->getParameter('view_id') == "related_content"))  {
-		  // Add link to groups view page
-		  $breadcrumb->addLink(Link::createFromRoute(t('Groups'), 'view.atwork_groups.page_1'));
-		  
-		  $url_param = str_replace('-', ' ', \Drupal::routeMatch()->getParameter('arg_0'));
-		  $group_data = '';
-		  
-		  try {
-		  	// Select like history from db
-		  	$connection = \Drupal::database();
-		  	$query = $connection->query("Select label, id from groups_field_data");
-		  	$group_data = $query->fetchAll();
-		  }
-		  catch(Exception $e) {
-		  	\Drupal::logger('type')->error($e->getMessage());
-		  }
-		  
-		  foreach($group_data as $group) {
-		  	if (strtolower($url_param) == strtolower($group->label)) {
-		  		$group_data = $group;
-		  	}
-		  }
-		  $group = Group::load(\Drupal::routeMatch()->getParameter('arg_0'));
-		  $breadcrumb->addLink(Link::createFromRoute(t($group_data->label), 'entity.group.canonical', ['group' => $group_data->id]));
-		  
-		  return $breadcrumb;
-		}
-		
-		// Get the route parameter for the current page
-		if(\Drupal::routeMatch()->getParameter('node')) {
-			$route_param = \Drupal::routeMatch()->getParameter('node');
-		} else if (\Drupal::routeMatch()->getParameter('group')) {
-			$route_param = \Drupal::routeMatch()->getParameter('group');
-		}
-		
-		//// Special handling based on node type aka bundle
-		//// NOTE use of the Link class
-		switch ( $route_param->bundle() ) {
-			case 'photos':
-				$gid = '';
-				foreach (GroupContent::loadByEntity($route_param) as $group_content) {
-					$gid = $group_content->getGroup()->id();
-				}
-				$group = Group::load($gid);
-				
-				// Add link to groups view page
-				$breadcrumb->addLink(Link::createFromRoute(t('Groups'), 'view.atwork_groups.page_1'));
+  /**
+   * {@inheritdoc}
+   */
+  public function build(RouteMatchInterface $route_match) {
+    // Define a new object of type Breadcrumb.
+    $breadcrumb = new Breadcrumb();
 
-				// Add Group Name 
-				$breadcrumb->addLink(Link::createFromRoute(t($group->label()), 'entity.group.canonical', ['group' => $group->id()]));
-				
-				// Add Group Galleries page. The param 'arg_0' is the value passed to the view for the contextual filter.
-				$breadcrumb->addLink(Link::createFromRoute(t('Photo Galleries'), 'view.related_content.page_2', ['arg_0' => strtolower(str_replace(' ', '-', $group->label()))]));
-		 		break;
-			case 'group_post':
-				$gid = '';
-				foreach (GroupContent::loadByEntity($route_param) as $group_content) {
-					$gid = $group_content->getGroup()->id();
-				}
-				$group = Group::load($gid);
+    // Build out the breadcrumb
+    // Add a link to the homepage as our first crumb.
+    $breadcrumb->addLink(Link ::createFromRoute('Home', '<front>'));
 
-				// Add link to groups view page
-				$breadcrumb->addLink(Link::createFromRoute(t('Groups'), 'view.atwork_groups.page_1'));
-				
-				// Add Group Name 
-				$breadcrumb->addLink(Link::createFromRoute(t($group->label()), 'entity.group.canonical', ['group' => $group->id()]));
-				
-				// Add Group Galleries page. The param 'arg_0' is the value passed to the view for the contextual filter.
-				$breadcrumb->addLink(Link::createFromRoute(t('Posts'), 'view.related_content.page_1', ['arg_0' => strtolower(str_replace(' ', '-', $group->label()))]));
-		 		break;
-			case 'atwork_groups':
-				// Add link to groups view page
-				$breadcrumb->addLink(Link::createFromRoute(t('Groups'), 'view.atwork_groups.page_1'));
-		 		break;
-		}
-		
-		// Don't forget to add cache control by a route.
-		// Otherwise all pages will have the same breadcrumb.
-		$breadcrumb->addCacheContexts(['route']);
-		
-		// Return breadcrumb
-		return $breadcrumb;
-	}
-	
+    // If this is a view page (ie related content view)
+    // handle it differently than a node type.
+    if ((\Drupal::routeMatch()->getParameter('view_id')) && (\Drupal::routeMatch()->getParameter('view_id') == "related_content")) {
+      // Add link to groups view page.
+      $url_param = str_replace('-', ' ', \Drupal::routeMatch()->getParameter('arg_0'));
+      $group_data = '';
+
+      try {
+        // Select like history from db.
+        $connection = \Drupal::database();
+        $query = $connection->query("Select label, id from groups_field_data");
+        $group_data = $query->fetchAll();
+      }
+      catch (Exception $e) {
+        \Drupal::logger('type')->error($e->getMessage());
+      }
+
+      foreach ($group_data as $group) {
+        if (strtolower($url_param) == strtolower($group->label)) {
+          $group_data = $group;
+        }
+      }
+      $group = Group::load(\Drupal::routeMatch()->getParameter('arg_0'));
+      $breadcrumb->addLink(Link::createFromRoute($group_data->label, 'entity.group.canonical', ['group' => $group_data->id]));
+
+      return $breadcrumb;
+    }
+
+    // Get the route parameter for the current page.
+    if (\Drupal::routeMatch()->getParameter('node')) {
+      $route_param = \Drupal::routeMatch()->getParameter('node');
+    }
+    elseif (\Drupal::routeMatch()->getParameter('group')) {
+      $route_param = \Drupal::routeMatch()->getParameter('group');
+    }
+    else {
+      $route_param = NULL;
+    }
+
+    // Special handling based on node type aka bundle
+    // NOTE use of the Link class.
+    switch ($route_param->bundle()) {
+      case 'photos':
+        $gid = '';
+        foreach (GroupContent::loadByEntity($route_param) as $group_content) {
+          $gid = $group_content->getGroup()->id();
+        }
+        $group = Group::load($gid);
+
+        // Add link to groups view page.
+        $breadcrumb->addLink(Link::createFromRoute(t('Groups'), 'view.atwork_groups.page_1'));
+
+        // Add Group Name.
+        $breadcrumb->addLink(Link::createFromRoute($group->label(), 'entity.group.canonical', ['group' => $group->id()]));
+
+        // Add Group Galleries page. The param 'arg_0' is the value
+        // passed to the view for the contextual filter.
+        $clean_label = strtolower(str_replace(' ', '-', $group->label()));
+        $breadcrumb
+          ->addLink(Link::createFromRoute(t('Photo Galleries'), 'view.related_content.page_2', ['arg_0' => $clean_label])
+        );
+        break;
+
+      case 'group_post':
+        $gid = '';
+        foreach (GroupContent::loadByEntity($route_param) as $group_content) {
+          $gid = $group_content->getGroup()->id();
+        }
+        $group = Group::load($gid);
+
+        // Add link to groups view page.
+        $breadcrumb->addLink(Link::createFromRoute(t('Groups'), 'view.atwork_groups.page_1'));
+
+        // Add Group Name.
+        $breadcrumb->addLink(Link::createFromRoute($group->label(), 'entity.group.canonical', ['group' => $group->id()]));
+
+        // Add Group Galleries page.
+        // The param 'arg_0' is the value passed
+        // to the view for the contextual filter.
+        $clean_label = strtolower(str_replace(' ', '-', $group->label()));
+        $breadcrumb->addLink(Link::createFromRoute(t('Posts'), 'view.related_content.page_1', ['arg_0' => $clean_label]));
+        break;
+
+      case 'atwork_groups':
+        // Add link to groups view page.
+        $breadcrumb->addLink(Link::createFromRoute(t('Groups'), 'view.atwork_groups.page_1'));
+        break;
+    }
+
+    // Don't forget to add cache control by a route.
+    // Otherwise all pages will have the same breadcrumb.
+    $breadcrumb->addCacheContexts(['route']);
+
+    // Return breadcrumb.
+    return $breadcrumb;
+  }
+
 }
