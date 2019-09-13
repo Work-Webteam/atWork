3eae356f300403b6d2a637f23b42c6b71d56cbad
twerdal <thayne.werdal@gov.bc.ca>
2019-09-13 16:55:11 +0000

    Logging updated - module now seems to be stable and ready for testing.

--- web/modules/custom/atwork_mail_send_update/src/Plugin/QueueWorker/NewsletterAddNewSubs.php
+++ web/modules/custom/atwork_mail_send_update/src/Plugin/QueueWorker/NewsletterAddNewSubs.php
@@ -28,8 +28,8 @@ class NewsletterAddNewSubs extends QueueWorkerBase {
   /**
    * {@inheritdoc}
    *
-   *   $item here will be an entity id for the subscription.
-   *   We can then turn off subscriptions for each entity id we have.
+   *   $item here will be an user id (uid) number.
+   *   We can then add subscriptions for each uid we have.
    */
   public function processItem($item) {
     // Take each user uid and sign them up.
@@ -37,7 +37,7 @@ class NewsletterAddNewSubs extends QueueWorkerBase {
       // Update subscription status by uid.
       $current_sub = User::load($item->uid);
       // Make sure this user has an actual email.
-      if ($current_sub->hasField('mail')) {
+      if ($current_sub->hasField('mail') && !empty($current_sub->getEmail)) {
         $mail = $current_sub->getEmail();
         $subscription_manager = \Drupal::service('simplenews.subscription_manager');
         // For now I am hard-coding this. In the future, we may want to
@@ -46,6 +46,10 @@ class NewsletterAddNewSubs extends QueueWorkerBase {
         $newsletter_id = 'atwork_newsletter';
 
         $subscription_manager->subscribe($mail, $newsletter_id, NULL, 'website');
+        Drupal::logger('atwork_mail_send_update')->notice('User @username has been subscribed to newsletter',
+          [
+            '@username' => $current_sub->getUsername(),
+          ]);
       }
       else {
         \Drupal::logger('atwork_mail_send_update')->notice('User @username does not have an email, not adding subscription.',
