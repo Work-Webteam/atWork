3d0e168a713d17a431d820e459f5035338c20c37
twerdal <thayne.werdal@gov.bc.ca>
2019-10-23 21:53:56 +0000

    Update to add css to group forms

--- web/modules/custom/atwork_group/js/atwork_group.js
+++ web/modules/custom/atwork_group/js/atwork_group.js
@@ -6,11 +6,7 @@
     attach: function (context, settings) {
       if (!$('#edit-field-themes--wrapper').hasClass('radio-update')) {
         $('#edit-field-themes--wrapper').addClass('radio-update');
-        $('.js-form-item-field-themes.form-item-field-themes .option').after(
-          '<div class="group-swatch"><div class="primary-col">Primary</div>' +
-          '<div class="secondary-col">Secondary</div><div class="link-col">' +
-          'Links</div></div>'
-      );
+        $('.js-form-item-field-themes.form-item-field-themes .option').after('<div class="group-swatch"><div class="primary-col">Primary</div><div class="secondary-col">Secondary</div><div class="link-col">Links</div></div>');
       }
     }
   }
